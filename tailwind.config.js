5c759cc6cac6ada5c930722824defbfc320b3067
Anish Chhetri <anishchhetri196@gmail.com>
2022-05-07 11:56:20 +0545

    Project 4

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ptSerif: "'PT Serif'",
+        poppins: "'Poppins'",
+      },
+      rotate: {
+        25: "25deg",
+      },
+
+      grayscale: {
+        50: "50%",
+      },
+    },
+  },
+  plugins: [],
+};
